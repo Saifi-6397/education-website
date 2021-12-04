a483ba31439ae3e81bc2d8185e0d08207c0602d8
Saifi-6397 <khaleelahmad3886@gmail.com>
2021-12-04 11:30:41 +0530

    final project

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(function() {
+
+    $('.home-slider .owl-carousel').owlCarousel({
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+            },
+            767: {
+                items: 2
+            },
+            991: {
+                items: 3
+            }
+
+        }
+    })
+
+    //course slider
+
+    $('.course .owl-carousel').owlCarousel({
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            991: {
+                items: 4
+            }
+
+        }
+
+    })
+
+    $('.count').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+    // $(window).on("scroll", function() {
+    //     if ($(window).scrollTop()) {
+    //         $("#my-navigation").addClass('scrollfix');
+    //     } else {
+    //         $("#my-navigation").removeClass('scrollfix');
+    //     }
+    // });
+
+
+    // AOS.init({
+    //     duration: 1000,
+    //     delay: 400,
+    // });
+
+
+    $.scrollUp({
+        scrollImg: true,
+        topDistance: '300',
+        topSpeed: 1000,
+    });
+
+
+
+})
