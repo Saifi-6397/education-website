316bd93aaaed1e64eed6a031302a7ce3b0df7acb
Saifi-6397 <khaleelahmad3886@gmail.com>
2021-12-16 12:06:07 +0530

    update

--- js/script.js
+++ js/script.js
@@ -78,6 +78,41 @@ $(function() {
         topSpeed: 1000,
     });
 
-
-
 })
+
+
+//form validation
+
+var user_name = document.getElementById("user_name");
+var user_email = document.getElementById("user_email");
+var user_sub = document.getElementById("user_sub");
+var return_val = 1;
+
+function Validation() {
+    if (user_name.value == "") {
+        document.getElementById("nameError").innerHTML = "* Please Enter Your Name";
+        return_val = 0;
+    } else {
+        document.getElementById("nameError").innerHTML = "";
+        return_val = 1;
+    }
+    if (user_email.value == "") {
+        document.getElementById("emailError").innerHTML = "* Please Enter Your Email";
+        return_val = 0;
+    } else {
+        document.getElementById("emailError").innerHTML = "";
+        return_val = 1;
+    }
+    if (user_sub.value == "") {
+        document.getElementById("subError").innerHTML = "* Please Enter Your Subject";
+        return_val = 0;
+    } else {
+        document.getElementById("subError").innerHTML = "";
+        return_val = 1;
+    }
+    if (return_val) {
+        return true;
+    } else {
+        return false;
+    }
+}
